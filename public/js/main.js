825f3d9c6eaa2a70192bb31b96e65eb55c176213
Fiona Carroll <fiona.carroll@me.com>
2022-05-11 12:58:26 -0700

    main page route

--- public/js/main.js
+++ public/js/main.js
@@ -7,12 +7,3 @@ document.querySelector(".btn-logout").addEventListener("click", async () => {
     console.error("Failed to logout.");
   }
 });
-// document.querySelector(".btn-logout").addEventListener("click", async () => {
-//   try {
-//     await fetch("/api/users/logout", { method: "POST" });
-//     document.location.replace("/home");
-//   } catch (error) {
-//     console.error(error);
-//     console.error("Failed to logout.");
-//   }
-// });
