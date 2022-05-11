72d278c05cb87559b5c74c8b061fb194be7f2b42
Fiona Carroll <fiona.carroll@me.com>
2022-05-11 14:14:40 -0700

    search and login buttons

--- models/User.js
+++ models/User.js
@@ -22,14 +22,7 @@ User.init(
       type: DataTypes.STRING,
       allowNull: false,
     },
-    email: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      unique: true,
-      validate: {
-        isEmail: true,
-      },
-    },
+
     password: {
       type: DataTypes.STRING,
       allowNull: false,
