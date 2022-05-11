7425ebecc0bdca16d6041756071a676e12eaa2ed
Zeth <zethsatentes@yahoo.com>
2022-05-11 13:02:05 -0700

    API routes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,8 +1,10 @@
 const router = require("express").Router();
 const usersRouter = require("./users-router");
-const exampleRouter = require("./example-router");
+const postRoutes = require('./post-router');
+const exampleRouter = require("./comment-router");
 
 router.use("/users", usersRouter);
+router.use('/post', postRoutes);
 router.use("/example", exampleRouter);
 
 module.exports = router;
