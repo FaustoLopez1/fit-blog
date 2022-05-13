e938335fa9ff47c6789779f55b0db8b271284d81
Fiona Carroll <fiona.carroll@me.com>
2022-05-12 21:11:22 -0700

    delete button handler and dashboard post modified

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -1,37 +1,46 @@
 async function newFormHandler(event) {
-    event.preventDefault();
-  
-    const title = document.getElementById('title').value;
-    const content = document.getElementById('content').value;
-    console.log(title,content);
-  
-    const response = await fetch(`/api/post/`, {
-      method: 'POST',
-      body: JSON.stringify({
-        title,
-        content
-      }),
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    });
-  
-    if (response.ok) {
-      document.location.replace('/dashboard');
-    } else {
-      alert(response.statusText);
-    }
-  };
-  
+  event.preventDefault();
+
+  const title = document.getElementById("title").value;
+  const content = document.getElementById("content").value;
+  console.log(title, content);
+
+  const response = await fetch(`/api/post/`, {
+    method: "POST",
+    body: JSON.stringify({
+      title,
+      content,
+    }),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+
+  if (response.ok) {
+    document.location.replace("/dashboard");
+  } else {
+    alert(response.statusText);
+  }
+}
+
 // document.querySelector('#new-post-form').addEventListener('submit', newFormHandler);
+const delButtonHandler = async (event) => {
+  if (event.target.hasAttribute("data-id")) {
+    const id = event.target.getAttribute("data-id");
 
+    const response = await fetch(`/api/blogs/${id}`, {
+      method: "DELETE",
+    });
 
-document.querySelector(".btn-post").addEventListener("click", newFormHandler); 
-  // try {
-  //   await fetch("/api/users/post", { method: "POST" });
-  //   document.location.replace("/single-post");
-  // } catch (error) {
-  //   console.error(error);
-  //   console.error("Failed to Post.");
-  // }
-;
+    if (response.ok) {
+      document.location.replace("/dashboard");
+    } else {
+      alert("Failed to delete project");
+    }
+  }
+};
+
+document.querySelector(".btn-post").addEventListener("click", newFormHandler);
+document
+  .querySelector(".post-list")
+  .addEventListener("click", delButtonHandler);
